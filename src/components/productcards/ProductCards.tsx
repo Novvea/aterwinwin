d1e4ad00b4e9ea7835e64f68ad84dd265251e8b0
Novvea <anna.c.novak@gmail.com>
2021-02-15 10:31:39 +0100

    Add buttons to productcard

--- src/components/productcards/ProductCards.tsx
+++ src/components/productcards/ProductCards.tsx
@@ -6,6 +6,11 @@ export const ProductCards = () => {
   return (
     <div className='productCardWrapper'>
       <h1>Låtsas att detta är ett produktkort</h1>
+      <div className='decisionButtonWrapper'>
+        <button>Nej tack!</button>
+        <button>Vill absolut ha!!!!</button>
+        <button> Gillar't </button>
+      </div>
     </div>
   )
 }
