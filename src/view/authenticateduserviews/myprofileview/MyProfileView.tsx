d0b2f946c3a0fc4e0fe669f353f1c7a8187acd41
Novvea <anna.c.novak@gmail.com>
2021-02-15 10:32:52 +0100

    What to do with my profileview

--- src/view/authenticateduserviews/myprofileview/MyProfileView.tsx
+++ src/view/authenticateduserviews/myprofileview/MyProfileView.tsx
@@ -5,6 +5,12 @@ export const MyProfileView = () => {
   return(
     <div>
       <h1>Detta är min profilsida</h1>
+      <ul>
+        <li>Lägg till info om användaren</li>
+        <li>Visa upplagda föremål</li>
+        <li>Visa gillade och supergillade föremål</li>
+        <li>Visa önskelistan</li>
+      </ul>
     </div>
   )
 }
